658ee1b011a9f5d6af9676f6ce20a2a874909207
kargmattis <115991382+kargmattis@users.noreply.github.com>
2023-12-12 11:00:04 +0100

    Add Adresse model to database

---
+++ src/database/adresse/adresse.ts
@@ -0,0 +1,54 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../database";
+
+class Adresse extends Model {
+  public adressId!: string;
+  public postleitzahl!: string;
+  public isthaupadresse!: boolean;
+  public straße!: string;
+  public hausnummer!: string;
+  public ort!: string;
+  public hausnummerzusatz!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Adresse.init(
+  {
+    adressId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true
+    },
+    postleitzahl: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    isthaupadresse: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    straße: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    hausnummer: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    ort: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    hausnummerzusatz: {
+      type: new DataTypes.STRING(128),
+      allowNull: true
+    }
+  },
+  {
+    tableName: "Adresse",
+    sequelize
+  }
+);
+
+export default Adresse;
