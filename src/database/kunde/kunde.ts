1cf7e525a07a2a8b49f6f387301be6ac392bafb6
kargmattis <115991382+kargmattis@users.noreply.github.com>
2023-12-12 11:00:15 +0100

    Add Kunde model to database

---
+++ src/database/kunde/kunde.ts
@@ -0,0 +1,54 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../database";
+
+class Kunde extends Model {
+  public kundenId!: string;
+  public email!: string;
+  public vorname!: string;
+  public passwort!: string;
+  public telefonnummer!: string;
+  public nachname!: string;
+  public zeitungsaboablaufdatum!: Date;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Kunde.init(
+  {
+    kundenId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true
+    },
+    email: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    vorname: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    nachname: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    passwort: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    telefonnummer: {
+      type: new DataTypes.STRING(128),
+      allowNull: false
+    },
+    zeitungsaboablaufdatum: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  },
+  {
+    tableName: "Kunde",
+    sequelize
+  }
+);
+
+export default Kunde;
