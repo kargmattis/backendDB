ea3913f931709d6dfb7fbf7501d28abeceb2ed94
kargmattis <115991382+kargmattis@users.noreply.github.com>
2023-12-12 10:49:12 +0100

    Add error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { type NextFunction, Request, Response } from "express";
 import { ProductController } from "./controller/productCotroller";
 import { sequelize } from "./database/database";
+import { errorChecking } from "./utilities/errorChecking";
 
 const app = express();
 const port = 3000;
@@ -12,7 +13,11 @@ app.use(express.json());
 sequelize.sync();
 // todo ende
 app.use("/api/v1", ProductController);
-
+// handelt errors die davor nicht abgefangen werden konnten
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  errorChecking(err);
+});
 // testing delete by production
 app.get("/", (req, res) => {
   res.send("Hello World!");
