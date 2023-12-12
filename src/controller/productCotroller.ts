01c4239b3df28b9dcdb1798f91049ee836b4e4f2
kargmattis <115991382+kargmattis@users.noreply.github.com>
2023-12-12 10:59:23 +0100

    Refactor productController routes and handle createProduct errors

--- src/controller/productCotroller.ts
+++ src/controller/productCotroller.ts
@@ -1,21 +1,27 @@
 import express from "express";
-import { createProduct } from "../database/product/createProduct";
+import { createProduct } from "../database/product/operations/createProduct";
 
 export const ProductController = express.Router();
 
-ProductController.get("/product", (req, res) => {
+ProductController.get("/product", (_req, res) => {
   res.send("Create Get product request");
 });
 
-ProductController.post("/product", (req, res) => {
-  res.send("Create Post product request");
-  createProduct();
+ProductController.post("/product", async (_req, res) => {
+  try {
+    await createProduct();
+    res.send("Create Post product request");
+  } catch (error) {
+    // Handle the error appropriately, don't just swallow it.
+    console.error(error);
+    res.status(500).send("An error occurred while creating the product");
+  }
 });
 
-ProductController.put("/product", (req, res) => {
+ProductController.put("/product", (_req, res) => {
   res.send("Create Put product request");
 });
 
-ProductController.delete("/product", (req, res) => {
+ProductController.delete("/product", (_req, res) => {
   res.send("Delete delete product request");
 });
