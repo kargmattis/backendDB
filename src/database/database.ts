33f25f18f4d5b0d8872810a8def4e46af53b6c4e
kargmattis <115991382+kargmattis@users.noreply.github.com>
2023-12-12 10:59:28 +0100

    Update database configuration

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from "sequelize";
 
-// export const sequelize = new Sequelize({
-//   dialect: "sqlite",
-//   storage: "productionEnvironment.sqlite",
-// });
-
-export const sequelize = new Sequelize("test_db", "root", "root", {
-  host: "localhost", // oder die IP-Adresse des Docker-Containers
-  port: 5432,
-  dialect: "postgres",
-  define: {
-    timestamps: false // für das Lesen von Legacy-Tabellen
-  }
+export const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "productionEnvironment.sqlite"
 });
+
+// export const sequelize = new Sequelize("test_db", "root", "root", {
+//   host: "localhost", // oder die IP-Adresse des Docker-Containers
+//   port: 5432,
+//   dialect: "postgres",
+//   define: {
+//     timestamps: false // für das Lesen von Legacy-Tabellen
+//   }
+// });
