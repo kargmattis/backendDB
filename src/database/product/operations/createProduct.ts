4ae3c64833e1e91564094c568100322dbe4bb3f7
kargmattis <115991382+kargmattis@users.noreply.github.com>
2023-12-13 11:59:11 +0100

    Add error checking and return new product in createProduct function

--- src/database/product/operations/createProduct.ts
+++ src/database/product/operations/createProduct.ts
@@ -1,13 +1,16 @@
+import { errorChecking } from "../../../utilities/errorChecking";
 import Product from "../product";
 
-export async function createProduct(): Promise<void> {
+export async function createProduct(): Promise<Product> {
   try {
     const newProduct = await Product.create({
       title: "Orangenmenü",
       price: 5.99,
       image: ""
     });
+    return newProduct;
   } catch (error) {
     console.error("Error creating product:", error);
+    throw errorChecking(error);
   }
 }
