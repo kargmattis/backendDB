83c7a8f8ef23b8c017950b2b8f7d924b5973ebe9
kargmattis <115991382+kargmattis@users.noreply.github.com>
2023-12-14 21:27:12 +0100

    Add relations between tables

--- src/database/relations.ts
+++ src/database/relations.ts
@@ -1,10 +1,27 @@
+import Adresse from "./adresse/addresse";
 import Bestellung from "./bestellung/bestellung";
 import Bestellungposition from "./bestellungsPosition/bestellungsPosition";
+import Kunde from "./kunde/kunde";
 import Product from "./product/product";
+import Paypal from "./zahlungsmöglichkeit/paypal";
 
+// m zu n Beziehung
 Product.belongsToMany(Bestellung, {
   through: Bestellungposition
 });
+// m zu n Beziehung
 Bestellung.belongsToMany(Product, {
   through: Bestellungposition
 });
+// 1 zu n Beziehung
+Adresse.hasMany(Bestellung);
+Bestellung.belongsTo(Adresse);
+// 1 zu n Beziehung
+Paypal.hasMany(Bestellung);
+Bestellung.belongsTo(Paypal);
+// 1 zu n Beziehung
+Kunde.hasMany(Paypal, { foreignKey: "kundenId" });
+Paypal.belongsTo(Kunde);
+// 1 zu n Beziehung
+Kunde.hasMany(Adresse, { foreignKey: "kundenId" });
+Adresse.belongsTo(Kunde);
