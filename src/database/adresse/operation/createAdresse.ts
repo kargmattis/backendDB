c12e120fc2de2759144552db9109655c4d9b9290
kargmattis <115991382+kargmattis@users.noreply.github.com>
2023-12-21 19:34:55 +0100

    Add createAdresse function for creating new addresses

---
+++ src/database/adresse/operation/createAdresse.ts
@@ -0,0 +1,15 @@
+import type { AdresseCreationAttributes } from "../../../global/types";
+import { errorChecking } from "../../../utilities/errorChecking";
+import Adresse from "../adresse";
+
+export async function createAdresse(
+  adressenData: AdresseCreationAttributes
+): Promise<Adresse> {
+  try {
+    const newAdresse = await Adresse.create(adressenData);
+    return newAdresse;
+  } catch (error) {
+    console.error("Error creating new Paypal record: ", error);
+    throw errorChecking(error);
+  }
+}
