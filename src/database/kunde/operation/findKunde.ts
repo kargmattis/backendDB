27c5d2675612704b8bff37a6562a78692e7ecdc5
2204Simon <sigl.simon@web.de>
2023-12-27 23:08:43 +0100

    Add login functionality to kundeController

--- src/database/kunde/operation/findKunde.ts
+++ src/database/kunde/operation/findKunde.ts
@@ -25,3 +25,16 @@ export async function findKunden(email: string) {
     errorChecking(error);
   }
 }
+
+// Funktion, um einen Kunden anhand der E-Mail zu suchen
+export async function findKundeByEmail(email: string): Promise<Kunde | null> {
+  try {
+    const kunde = await Kunde.findOne({ where: { email } });
+    if (kunde !== null) {
+      return kunde;
+    }
+    throw new CustomError(ErrorHandle.NotFound, "Kunde nicht gefunden");
+  } catch (error) {
+    throw errorChecking(error);
+  }
+}
