4f062146c6b21bf9814fd6048364549c2ac8459d
ThorstenLicht <114730064+ThorstenLicht@users.noreply.github.com>
2024-01-06 16:52:53 +0100

    Image paths

--- src/testing/ProduktArray.ts
+++ src/testing/ProduktArray.ts
@@ -5,169 +5,163 @@ const Products: Array<ProduktCreationAttributes> = [
   {
     titel: "Baguette",
     preis: 2.99,
-    bild: "BaguetteAlone",
+    bild: "Food/BaguetteAlone.webp",
     sparte: "Food"
   },
   {
     titel: "Käsebaguette",
     preis: 3.59,
-    bild: "BaguetteYellow",
+    bild: "Food/BaguetteGelb.webp",
     sparte: "Food"
   },
   {
     titel: "Veganes Käsebaguette",
     preis: 3.59,
-    bild: "KäseBaguette",
+    bild: "Food/käseBaguett.webp",
     sparte: "Food"
   },
   {
     titel: "Brezel",
     preis: 1.49,
-    bild: "Brezel",
+    bild: "Food/Brezel.webp",
     sparte: "Food"
   },
   {
     titel: "Türkisches Menemen",
     preis: 7.99,
-    bild: "Menemen",
+    bild: "Food/Menemen.webp",
     sparte: "Food"
   },
   {
     titel: "Brötchen",
     preis: 1.29,
-    bild: "Brötchen",
+    bild: "Food/Brötchen.webp",
     sparte: "Food"
   },
   {
     titel: "Croissant",
     preis: 1.79,
-    bild: "Croissant",
-    sparte: "Food"
-  },
-  {
-    titel: "Baguette",
-    preis: 2.99,
-    bild: "BaguetteAlone",
+    bild: "Food/Croissant.webp",
     sparte: "Food"
   },
   {
     titel: "Fresh Chicken",
     preis: 3.79,
-    bild: "HamburgerBrötchen",
+    bild: "Food/HamburgerBrötchen.webp",
     sparte: "Food"
   },
   {
     titel: "Veganer Toast",
     preis: 3.59,
-    bild: "Toast",
+    bild: "Food/Toast.webp",
     sparte: "Food"
   },
   {
     titel: "Toast mit Rinderpatty",
     preis: 1.79,
-    bild: "BelegtesBrötchen",
+    bild: "Food/belegtesBrötchen.webp",
     sparte: "Food"
   },
   {
     titel: "Espresso",
     preis: 1.99,
-    bild: "Espresso",
+    bild: "Drinks/Espresso.webp",
     sparte: "Drink"
   },
   {
     titel: "Schwarzer Kaffee",
     preis: 5.99,
-    bild: "Kaffee",
+    bild: "Drinks/Kaffee.webp",
     sparte: "Drink"
   },
   {
     titel: "Latte Machiatto",
     preis: 5.99,
-    bild: "LatteMachiatto",
+    bild: "Drinks/Latte Machiatto.webp",
     sparte: "Drink"
   },
   {
     titel: "Orangensaft",
     preis: 5.99,
-    bild: "Orangensaft",
+    bild: "Drinks/Orangensaft.webp",
     sparte: "Drink"
   },
   {
     titel: "Früchtetee",
     preis: 5.99,
-    bild: "Früchtetee",
+    bild: "Drinks/Früchtetee.webp",
     sparte: "Drink"
   },
   {
     titel: "Wasser",
     preis: 5.99,
-    bild: "Wasser",
+    bild: "Drinks/Wasserglas.webp",
     sparte: "Drink"
   },
   {
     titel: "Zitronentee",
     preis: 5.99,
-    bild: "Zitronentee",
+    bild: "Drinks/Zitronentee.webp",
     sparte: "Drink"
   },
   {
     titel: "Heiße Schokolade",
     preis: 5.99,
-    bild: "HeißeSchokolade",
+    bild: "Drinks/heißeSchokolade.webp",
     sparte: "Drink"
   },
   {
     titel: "Das kleine Osterfrühstück",
     preis: 5.99,
-    bild: "Osterfruehstueck",
+    bild: "Food/Osterfruehstueck.webp",
     sparte: "Menu"
   },
   {
     titel: "Bayrisches Menü",
     preis: 7.99,
-    bild: "Bayrisches_Essen",
+    bild: "Food/bayrisches_Essen.webp",
     sparte: "Menu"
   },
   {
     titel: "Wecken mit Kaffee",
     preis: 6.99,
-    bild: "BrötchenMitKaffe",
+    bild: "Food/BötchenmitKaffee.webp",
     sparte: "Menu"
   },
   {
     titel: "Obstmenü",
     preis: 5.99,
-    bild: "BrotEiTeeUndObst",
+    bild: "Food/BrotEiTeeUndObst.webp",
     sparte: "Menu"
   },
   {
     titel: "Eiermenü",
     preis: 5.99,
-    bild: "EigetränkUndEierbrot",
+    bild: "Food/EigetränkUndEierbrot.webp",
     sparte: "Menu"
   },
   {
     titel: "Klassisches Menü",
     preis: 5.99,
-    bild: "KaffeeMitEiUndBrot",
+    bild: "Food/KaffeeMitEiUndBrot.webp",
     sparte: "Menu"
   },
   {
     titel: "Veggiemenü",
     preis: 5.99,
-    bild: "OSaftMitFrüchtebrot",
+    bild: "Food/OSaftMitFrüchtebrot.webp",
     sparte: "Menu"
   },
   {
     titel: "Orangenmenü",
     preis: 5.99,
-    bild: "OSaftUndOrangenbrot",
+    bild: "Food/OSaftUndOrangenbrot.webp",
     sparte: "Menu"
   },
   {
     titel: "Zeitung",
     preis: 1.0,
-    bild: "OSaftUndOrangenbrot", //TODO: Bild austauschen
+    bild: "Food/OSaftUndOrangenbrot.webp", //TODO: Bild austauschen
     sparte: "Newspaper"
   }
 ];
