978feff18cbb1bf30834050d1a72f9def73793df
kargmattis <115991382+kargmattis@users.noreply.github.com>
2024-02-13 16:51:44 +0100

    Remove unused code in adresseController

--- src/controller/adresseController.ts
+++ src/controller/adresseController.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import type { Request } from "express";
-import CustomError from "../utilities/error";
 import { createAdresse } from "../database/adresse/operation/createAdresse";
 import type { AdresseCreationAttributes } from "../global/types";
 import {
@@ -34,33 +33,33 @@ AdresseController.post("/adresse", async (req: Request, res) => {
   }
 });
 
-AdresseController.put("/adresse", async (req, res) => {
-  await putAdresse(req.body)
-    .then((adresse) => {
-      res.status(200).json(adresse);
-    })
-    .catch((error) => {
-      const customError = errorValidation(error);
-      res.status(customError.statusCode).send(customError.message);
-    });
-});
+// AdresseController.put("/adresse", async (req, res) => {
+//   await putAdresse(req.body)
+//     .then((adresse) => {
+//       res.status(200).json(adresse);
+//     })
+//     .catch((error) => {
+//       const customError = errorValidation(error);
+//       res.status(customError.statusCode).send(customError.message);
+//     });
+// });
 
-AdresseController.delete("/adresse/:kundenId", async (req, res) => {
-  try {
-    const deletedCount = await Adresse.destroy({
-      where: { kundenId: req.params.kundenId }
-    });
-    console.log(deletedCount);
-    if (deletedCount > 0) {
-      res.status(200).send("Adresse deleted");
-    } else {
-      res.status(404).send("Adresse not found");
-    }
-  } catch (error) {
-    const customError = errorValidation(error);
-    res.status(customError.statusCode).send(customError.message);
-  }
-});
+// AdresseController.delete("/adresse/:kundenId", async (req, res) => {
+//   try {
+//     const deletedCount = await Adresse.destroy({
+//       where: { kundenId: req.params.kundenId }
+//     });
+//     console.log(deletedCount);
+//     if (deletedCount > 0) {
+//       res.status(200).send("Adresse deleted");
+//     } else {
+//       res.status(404).send("Adresse not found");
+//     }
+//   } catch (error) {
+//     const customError = errorValidation(error);
+//     res.status(customError.statusCode).send(customError.message);
+//   }
+// });
 
 AdresseController.get("/adressen/:kundeId", async (req: Request, res) => {
   try {
